78418c81bb7b60eda5c5ef027121ff71ba92a7dc
Haris <mhaaris0815@gmail.com>
2021-12-09 23:47:11 +0530

    new changes

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -4,8 +4,10 @@ const Homepage = () => {
   return (
     <div className="homepage-container ">
       <h1>This is a container for the blog page....</h1>
-      <h1>This is Heading in main container</h1>
+      <h1>This is Heading in main the container</h1>
       <h1>Hello</h1>
+
+      <h1>This is a component in homepage</h1>
       <h1>This is a component in homepage</h1>
       {/* <AuthorImage /> */}
     </div>
