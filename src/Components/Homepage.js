5e09f650f95de4d598fd46d1fdad73753f88adc2
Haris <mhaaris0815@gmail.com>
2021-12-11 23:48:08 +0530

    changed blog cards

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,5 +1,6 @@
 import "./Styles/Homepage.css";
 import { Card, Carousel } from "antd";
+import { margin } from "@mui/system";
 const { Meta } = Card;
 
 // import AuthorImage from "./AuthorImage";
@@ -7,21 +8,23 @@ const Homepage = () => {
   // Array of objects for blog
   const blogs = [
     {
-      vacationName: "United Kingdom",
-      descriptionLondon:
+      name: "United Kingdom",
+      description:
         "This is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacation",
       titleUK: "Vacation",
     },
 
     {
       name: "vacation",
-      description: "This is my blog for the vacation",
+      description:
+        "This is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacation",
       title: "Vacation",
     },
 
     {
       name: "vacation",
-      description: "This is my blog for the vacation",
+      description:
+        "This is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacation",
       title: "Vacation",
     },
   ];
@@ -31,30 +34,30 @@ const Homepage = () => {
       <div className="homepage-container  ">
         <h1>This is a container for the blog page....</h1>
 
-        <div className="blog-card">
-          <Card
-            hoverable
-            style={{ width: 400, borderRadius: "5px" }}
-            cover={
-              <>
-                <div className="d-flex justify-content-center align-items-center">
-                  <span>
-                    <i class="fas fa-plane" style={{ fontSize: "20px" }}></i>
-                  </span>
-                  <h4 style={{ margin: "5px" }}>Visit to United Kingdom</h4>
-                </div>
-              </>
-            }
-          >
-            {blogs.map((blog) => {
-              return (
-                <Meta
-                  title={blog.vacationName}
-                  description={blog.descriptionLondon}
-                />
-              );
-            })}
-          </Card>
+        <div className="blog-card d-flex justify-content-center ">
+          {blogs.map((blog) => {
+            return (
+              <Card
+                hoverable
+                style={{ width: 400, borderRadius: "15px/45px", margin: "10px" }}
+                cover={
+                  <>
+                    <div className="d-flex justify-content-center align-items-center">
+                      <span>
+                        <i
+                          class="fas fa-plane"
+                          style={{ fontSize: "20px" }}
+                        ></i>
+                      </span>
+                      <h4 style={{ margin: "5px" }}>Visit to United Kingdom</h4>
+                    </div>
+                  </>
+                }
+              >
+                <Meta title={blog.name} description={blog.description} />
+              </Card>
+            );
+          })}
         </div>
       </div>
     </>
