d78c5280d82dc8b454f5d54c15e9ea8466bc4b68
Haris <mhaaris0815@gmail.com>
2021-12-11 19:03:50 +0530

    made a card with ant design library

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,11 +1,15 @@
 import "./Styles/Homepage.css";
+import { Card } from "antd";
+const { Meta } = Card;
+
 // import AuthorImage from "./AuthorImage";
 const Homepage = () => {
+  // Array of objects for blog
   const blogs = [
     {
-      name: "vacation",
-      description: "This is my blog for the vacation",
-      title: "Vacation",
+      vacationName: "United Kingdom",
+      descriptionLondon: "This is my blog for the vacation",
+      titleUK: "Vacation",
     },
 
     {
@@ -22,18 +26,28 @@ const Homepage = () => {
   ];
 
   return (
-    <div className="homepage-container ">
-      <h1>This is a container for the blog page....</h1>
-      <h1>This is Heading in main the container</h1>
-      <h1>Hello</h1>
+    <>
+      <div className="homepage-container  ">
+        <h1>This is a container for the blog page....</h1>
 
-      <h1>This is a component in homepage</h1>
-      <h1>This is a component in homepage</h1>
-      {blogs.map((blog) => {
-        return <div>{blog.name}</div>;
-      })}
-      {/* <AuthorImage /> */}
-    </div>
+        <div className="blog-card">
+          <Card
+            hoverable
+            style={{ width: 240, borderRadius: "5px" }}
+            cover={<h4>Unexpected trip to United Kingdom</h4>}
+          >
+            {blogs.map((blog) => {
+              return (
+                <Meta
+                  title={blog.vacationName}
+                  description={blog.descriptionLondon}
+                />
+              );
+            })}
+          </Card>
+        </div>
+      </div>
+    </>
   );
 };
 
