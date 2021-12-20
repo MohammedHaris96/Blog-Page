bf6232d21efd79c52a7d401c4899fde5fe957280
Haris <mhaaris0815@gmail.com>
2021-12-20 23:24:10 +0530

    new changes

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -33,12 +33,6 @@ const Homepage = () => {
     <>
       <div className="homepage-container  ">
         <h1>Heading------- for homepage(title). The blog page</h1>
-
-        <h1>Heading------- for homepage(title). The blog page</h1>
-        
-        <h1>Heading------- for homepage(title). The blog page</h1>
-        <h1>Heading------- for homepage(title). The blog page</h1>
-        <h1>Heading------- for homepage(title). The blog page</h1>
         <div className="blog-card d-flex justify-content-center ">
           {blogs.map((blog) => {
             return (
