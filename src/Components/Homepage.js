86b9a29b26b591701afdc7944e35f543684e28f5
Haris <mhaaris0815@gmail.com>
2021-12-29 23:51:44 +0530

    new changes

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -21,6 +21,14 @@ const Homepage = () => {
     },
 
     {
+      name: "Spain",
+      description:
+        "This is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacationThis is my blog for the vacation",
+      title: "Holidays",
+      id: 5,
+    },
+
+    {
       name: "United Kingdom",
       description:
         "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
