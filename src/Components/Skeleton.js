eb01add4c03cc4eb4577cdad2bf778b7aeb86ada
Haris <mhaaris0815@gmail.com>
2022-01-08 23:52:01 +0530

    new chnge

--- src/Components/Skeleton.js
+++ src/Components/Skeleton.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Skeleton = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
 
-export default Skeleton
+export default Skeleton;
