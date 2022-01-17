d3b8c9ab36f45eecf260b7ebeef84dcd9d30f7e4
Haris <mhaaris0815@gmail.com>
2022-01-17 22:58:59 +0530

    new

--- src/Components/AboutPage.js
+++ src/Components/AboutPage.js
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 const AboutPage = () => {
   return (
     <div className="homepage-container  ">
-      <h1>This my about page</h1>
+      <h1>This my about page </h1>
 
       <Footer />
     </div>
