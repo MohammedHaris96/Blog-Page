c89c1b256d4e9661933c6f36be379bdab5a86876
Haris <mhaaris0815@gmail.com>
2022-01-23 22:30:29 +0530

    new change

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,9 @@ import "./Styles/Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <h1>Footer Blog Page  New</h1>
+      <h1>Footer Blog Page New</h1>
+      <h1>Footer Blog Page New</h1>
+      <h1>Footer Blog Page New</h1>
     </div>
   );
 };
