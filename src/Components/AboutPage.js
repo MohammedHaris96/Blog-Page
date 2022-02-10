fcd14587aba8e43b6b15649d6b2cc1f9eb1ef098
Haris <mhaaris0815@gmail.com>
2022-02-10 23:36:35 +0530

    minor change

--- src/Components/AboutPage.js
+++ src/Components/AboutPage.js
@@ -5,6 +5,7 @@ const AboutPage = () => {
   return (
     <div className="homepage-container  ">
       <h1>This my about page </h1>
+      <h1>addded a new text</h1>
 
       <Footer />
     </div>
