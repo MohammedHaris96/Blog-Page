e43d02c173f520ba1ec4b557f726008a4e9a868b
Haris <mhaaris0815@gmail.com>
2022-09-16 23:52:02 +0530

    minor change

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -63,7 +63,7 @@ const Homepage = () => {
           <div className="text-content-name">
             I am Har<span className="name-auth">i</span>s
           </div>
-          <div className="text-profession">Software DeveloperDeveloper</div>
+          <div className="text-profession">Software DeveloperDeveloperDeveloperDeveloper</div>
         </div>
         <div className="homepage-img">
           <img src={userImage} className="user-img" alt="user-image"></img>
